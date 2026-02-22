cad174b16828bde148b1b7f27d1574a1afa7136f
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-02-22 19:37:20 +0100

    Add useState and useEffect hooks to manage the cards' state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Card } from "./components/Card";
 import { GameHeader } from "./components/GameHeader";
 
@@ -21,12 +22,32 @@ const cardValues = [
 ];
 
 function App() {
+  const [cards, setCards] = useState([]);
+
+  const initializeGame = () => {
+    ///Shuffle Cards
+    console.log(cardValues);
+
+    const finalCards = cardValues.map((value, index) => ({
+      id: index,
+      value,
+      isFlipped: false,
+      isMatched: false,
+    }));
+    console.log(finalCards);
+    setCards(finalCards);
+  };
+
+  useEffect(() => {
+    initializeGame();
+  }, []);
+
   return (
     <div className="app">
       <GameHeader score={3} moves={10} />
 
       <div className="cards-grid">
-        {cardValues.map((card) => (
+        {cards.map((card) => (
           <Card card={card} />
         ))}
       </div>
